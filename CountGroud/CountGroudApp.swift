05afda7469a96382e511e47115eb3b25c96b3d43
zhenheng <zhenheng@tencent.com>
2022-04-29 10:21:12 +0800

    Initial Commit

---
+++ CountGroud/CountGroudApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountGroudApp.swift
+//  CountGroud
+//
+//  Created by sofent on 2022/4/29.
+//
+
+import SwiftUI
+
+@main
+struct CountGroudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

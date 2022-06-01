920baa1c3f24865d84dfc28f44aff44305056d4d
zhenheng <zhenheng@tencent.com>
2022-06-01 12:03:03 +0800

    优化touch逻辑

--- CountGround/GameScene.swift
+++ CountGround/GameScene.swift
@@ -12,6 +12,7 @@ class GameScene: SKScene {
     let background = SKSpriteNode(imageNamed: "backgroud")
     var zeroNode = SKNode()
     var selectedNode = SKNode()
+    var snodePos = CGPoint.zero
     var puzzle =  Array(1...16)
     var shape = SKShapeNode()
     required init?(coder aDecoder: NSCoder) {
@@ -26,7 +27,7 @@ class GameScene: SKScene {
         self.background.scale(to: size)
         self.background.anchorPoint = .zero
         self.background.zPosition = -1
-      
+        
         // 2
         self.addChild(background)
         let  shapeSize = CGSize(width: size.width*0.8, height: size.width*0.8)
@@ -37,8 +38,8 @@ class GameScene: SKScene {
         shape.position = CGPoint(x: (size.width-shapeSize.width)/2,y: (size.height-shapeSize.width)/2)
         self.shape=shape
         addChild(shape)
-       
-       
+        
+        
         
     }
     
@@ -52,7 +53,7 @@ class GameScene: SKScene {
     func initPuzzle(){
         var puzzle =  Array(1...16)
         puzzle[15] = 0
-       
+        
         for _ in 1...128 {
             let index = Int.random(in: 0...3)
             move(Direction.allCases[index], puzzle: &puzzle)
@@ -108,55 +109,73 @@ class GameScene: SKScene {
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         selectedNode = SKNode()
-        for touch in touches {
+        if let touch = touches.first {
             let p = touch.location(in: self)
             self.nodes(at: p).forEach{ n in
                 if let name=n.name,name != "background"{
                     if n != zeroNode && abs(n.position.x-zeroNode.position.x) < (n.frame.width+1) && abs(n.position.y-zeroNode.position.y) < (n.frame.height+1) && (n.position.x == zeroNode.position.x || n.position.y == zeroNode.position.y){
                         print( abs(n.position.x-zeroNode.position.x),abs(n.position.y-zeroNode.position.y) )
                         selectedNode = n
-                       
+                        snodePos = p
+                        selectedNode.removeAllActions()
+                        let sequence = SKAction.sequence([SKAction.rotate(byAngle: degToRad(degree: -4.0), duration: 0.1),
+                                                          SKAction.rotate(byAngle: 0.0, duration: 0.1),
+                                                          SKAction.rotate(byAngle: degToRad(degree: 4.0), duration: 0.1)])
+                        selectedNode.run(SKAction.repeatForever(sequence))
                     }
                 }
             }
         }
     }
     
-   /* override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
-            let p = touch.location(in: self)
-            self.nodes(at: p).forEach{ n in
-                if let name=n.name,name != "background"{
-                    if n != zeroNode {
-                        selectedNode = n
-                    }
-                }
-            }
-        }
-    }*/
+    func degToRad(degree: Double) -> CGFloat {
+        return CGFloat(degree / 180.0 * .pi)
+    }
+    
+    /* override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+     for touch in touches {
+     let p = touch.location(in: self)
+     self.nodes(at: p).forEach{ n in
+     if let name=n.name,name != "background"{
+     if n != zeroNode {
+     selectedNode = n
+     }
+     }
+     }
+     }
+     }*/
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch in touches {
+        selectedNode.removeAllActions()
+        selectedNode.run(SKAction.rotate(toAngle: 0.0, duration: 0.1))
+        if selectedNode.name == nil {
+            return
+        }
+        if let touch = touches.first {
             let p = touch.location(in: self)
-            self.nodes(at: p).forEach{ n in
-                if n == zeroNode && selectedNode.name != nil {
-                    let np = selectedNode.position
-                    selectedNode.position = zeroNode.position
-                    zeroNode.position = np
-                    puzzle.swapAt(puzzle.firstIndex(of: 0)!,puzzle.firstIndex(of: Int(selectedNode.name!)!)!)
-                    var answer = Array(1...16)
-                    answer[15] = 0
-                    print(puzzle)
-                    if puzzle == answer {
-                        let loseAction = SKAction.run() { [weak self] in
-                            guard let `self` = self else { return }
-                            let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
-                            let gameOverScene = GameOverScene(size: self.size)
-                            self.view?.presentScene(gameOverScene, transition: reveal)
-                        }
-                        self.run(loseAction)
-                        
+            let offset = zeroNode.position - snodePos
+            let offset2 = zeroNode.position - p
+            print(offset.length(),offset2.length())
+            if  offset.length() >= offset2.length() || abs(offset.length()-offset2.length()) < 3{
+                let snodeMove = SKAction.move(to: zeroNode.position, duration: 0.1)
+                let znodeMove = SKAction.move(to: selectedNode.position, duration: 0.1)
+                selectedNode.run(snodeMove)
+                zeroNode.removeAllActions()
+                zeroNode.run(znodeMove)
+                //zeroNode.position = np
+                puzzle.swapAt(puzzle.firstIndex(of: 0)!,puzzle.firstIndex(of: Int(selectedNode.name!)!)!)
+                var answer = Array(1...16)
+                answer[15] = 0
+                print(puzzle)
+                if puzzle == answer {
+                    let loseAction = SKAction.run() { [weak self] in
+                        guard let `self` = self else { return }
+                        let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                        let gameOverScene = GameOverScene(size: self.size)
+                        self.view?.presentScene(gameOverScene, transition: reveal)
                     }
+                    self.run(loseAction)
+                    
                 }
             }
         }
@@ -166,51 +185,79 @@ class GameScene: SKScene {
 }
 
 extension GameScene: SKPhysicsContactDelegate {
-  override func update(_ currentTime: TimeInterval) {
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
     
-  }
-  
-  func didBegin(_ contact: SKPhysicsContact) {
-      if contact.bodyA.node == selectedNode || contact.bodyB.node == selectedNode{
-          
-      }
+    func didBegin(_ contact: SKPhysicsContact) {
+        if contact.bodyA.node == selectedNode || contact.bodyB.node == selectedNode{
+            
+        }
     }
 }
 
-class GameOverScene: SKScene {
-    override init(size: CGSize) {
-    super.init(size: size)
-    
-    // 1
-        backgroundColor = SKColor.systemPink
-    
-    // 2
-    let message = "You Won!"
-    
-    // 3
-    let label = SKLabelNode(fontNamed: "Chalkduster")
-    label.text = message
-    label.fontSize = 40
-    label.fontColor = SKColor.black
-    label.position = CGPoint(x: size.width/2, y: size.height/2)
-    addChild(label)
-    
-    // 4
-    run(SKAction.sequence([
-      SKAction.wait(forDuration: 3.0),
-      SKAction.run() { [weak self] in
-        // 5
-        guard let `self` = self else { return }
-        let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
-        let scene = GameScene(size: size)
-        self.view?.presentScene(scene, transition:reveal)
-      }
-      ]))
+func +(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x + right.x, y: left.y + right.y)
+}
+
+func -(left: CGPoint, right: CGPoint) -> CGPoint {
+  return CGPoint(x: left.x - right.x, y: left.y - right.y)
+}
+
+func *(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x * scalar, y: point.y * scalar)
+}
+
+func /(point: CGPoint, scalar: CGFloat) -> CGPoint {
+  return CGPoint(x: point.x / scalar, y: point.y / scalar)
+}
+
+
+extension CGPoint {
+  func length() -> CGFloat {
+    return sqrt(x*x + y*y)
   }
   
-  // 6
-  required init(coder aDecoder: NSCoder) {
-    fatalError("init(coder:) has not been implemented")
+  func normalized() -> CGPoint {
+    return self / length()
   }
+}
+
+
+class GameOverScene: SKScene {
+    override init(size: CGSize) {
+        super.init(size: size)
+        
+        // 1
+        backgroundColor = SKColor.systemPink
+        
+        // 2
+        let message = "You Won!"
+        
+        // 3
+        let label = SKLabelNode(fontNamed: "Chalkduster")
+        label.text = message
+        label.fontSize = 40
+        label.fontColor = SKColor.black
+        label.position = CGPoint(x: size.width/2, y: size.height/2)
+        addChild(label)
+        
+        // 4
+        run(SKAction.sequence([
+            SKAction.wait(forDuration: 3.0),
+            SKAction.run() { [weak self] in
+                // 5
+                guard let `self` = self else { return }
+                let reveal = SKTransition.flipHorizontal(withDuration: 0.5)
+                let scene = GameScene(size: size)
+                self.view?.presentScene(scene, transition:reveal)
+            }
+        ]))
+    }
+    
+    // 6
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
